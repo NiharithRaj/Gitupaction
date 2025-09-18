50a34f052c537ce9ba063bee19b19a040d311d53
abi <mailtoniharithraj@gmail.com>
2025-09-18 20:50:07 +1000

    init commit

---
+++ Action/Action/ActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionApp.swift
+//  Action
+//
+//  Created by Raj on 18/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct ActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
